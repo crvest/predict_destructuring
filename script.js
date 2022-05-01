0eab82f712e8ce10501a0a0951180da31c1f2b3b
crvest <chad.vest@gmail.com>
2022-05-01 13:28:46 -0700

    predict_destructuring complete

---
+++ script.js
@@ -0,0 +1,80 @@
+// problem 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+// prediction
+// Tesla
+// Mercedes
+// prediciton correct
+
+
+// problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+// prediction
+// error: name not defined
+// prediction correct
+
+
+// problem 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+// prediction
+// error: const var is immutable
+// prediction wrong
+// answer: 12345
+// undefined        no property of person named password
+
+
+// problem 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// prediction
+// false        2 != 5
+// true         2 = 2
+// prediction  correct
+
+
+// problem 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+// prediction
+// value
+// [1,5,1,8,3,3]
+// 1
+// undefined
+// prediction wrong
+// answer: console.log(willThisWork); logs index[1] of secondKey which is 5
